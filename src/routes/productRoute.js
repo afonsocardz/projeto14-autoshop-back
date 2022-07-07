b5658dda19608e84a181a8036b49fd1aa1df53f8
dr1co <dricomatoba@hotmail.com>
2022-07-07 18:45:50 -0300

    added GET on route /products

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createProduct, createBrand } from "../controllers/productController.js";
+import { createProduct, createBrand, getAllProducts } from "../controllers/productController.js";
 import validateBrand from "../middlewares/validateBrandMiddleware.js";
 import validateProduct from "../middlewares/validateProductMiddleware.js";
 
 const productRoute = Router();
 
+productRoute.get("/products", getAllProducts);
 productRoute.post("/product", validateProduct, createProduct);
 productRoute.post("/product/brand", validateBrand, createBrand);
 
