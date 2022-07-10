39e0e647d258d04e1f30af910020741ad89555b9
Afonso Cardozo Cruz <afonso.desenvolvimento@gmail.com>
2022-07-09 23:14:11 -0300

    remove: delete category and brand routes and controllers

--- src/routes/productRoute.js
+++ src/routes/productRoute.js
@@ -1,7 +1,5 @@
 import { Router } from "express";
-import { createProduct, createBrand, createCategory, getAllProducts, getProductById } from "../controllers/productController.js";
-import validateBrand from "../middlewares/validateBrandMiddleware.js";
-import validateCategory from "../middlewares/validateCategoryMiddleware.js";
+import { createProduct, getAllProducts, getProductById } from "../controllers/productController.js";
 import validateProduct from "../middlewares/validateProductMiddleware.js";
 
 const productRoute = Router();
@@ -9,7 +7,5 @@ const productRoute = Router();
 productRoute.get("/products", getAllProducts);
 productRoute.get("/products/:productId", getProductById)
 productRoute.post("/product", validateProduct, createProduct);
-productRoute.post("/product/brand", validateBrand, createBrand);
-productRoute.post("/product/category", validateCategory, createCategory);
 
 export { productRoute };
