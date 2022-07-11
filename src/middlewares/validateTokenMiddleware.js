bef6e55974674876a1715d0cacfdeae0f14d662a
dr1co <98858768+dr1co@users.noreply.github.com>
2022-07-11 10:38:52 -0300

    Delete validateTokenMiddleware.js

--- src/middlewares/validateTokenMiddleware.js
+++
@@ -1,7 +0,0 @@
-export async function validateToken(req, res, next) {
-    const { authorization } = req.headers;
-    const token = authorization?.replace('Bearer ', '');
-    //validação do token...
-    res.locals.token = token
-    next();
-}
